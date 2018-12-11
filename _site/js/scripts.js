7cb98f79983eb2cd7c2a9b62d33c4b8ec17d62a0
Crecker2 <joshua.fayer@gmail.com>
2018-12-11 11:26:53 +0000

    added show/hide functionality

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,3 +1,18 @@
+function showhidecontent(e){
+  if(!$(".articlebody .show-hide-button button").data("toggle")){
+    $(".articlebody p, .articlebody li").css("filter", "blur(5px)");
+    $(".articlebody p, .articlebody li").data("toggle", true);
+    $(".articlebody .show-hide-button button").data("toggle", true);
+    $(".articlebody .show-hide-button button").text("show all content");
+
+  } else {
+    $(".articlebody p, .articlebody li").css("filter", "none");
+    $(".articlebody p, .articlebody li").data("toggle", false);
+    $(".articlebody .show-hide-button button").data("toggle", false);
+    $(".articlebody .show-hide-button button").text("hide all content");
+  }
+}
+
 $(document).ready(function(){
   $('.articlebody h1').filter('[id]').each(function(a,b){
       $(b).append(' <a href=#'+$(b).attr("id")+'>#</a>');
@@ -5,5 +20,16 @@ $(document).ready(function(){
 
   $('.articlebody h2').filter('[id]').each(function(a,b){
       $(b).append(' <a href=#'+$(b).attr("id")+'>#</a>');
+  });
+
+  $(".articlebody p, .articlebody li").click(function(e){
+  	e.stopPropagation();
+  	if(!$(this).data('toggle')){
+  		$(this).css("filter","blur(5px)");
+  		$(this).data('toggle',true);
+  	} else {
+  		$(this).css("filter","none");
+  		$(this).data('toggle',false);
+  	}
   });
 });
